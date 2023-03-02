cf4049640fbf49f23da35ae51c28637e3467e89a
hitender bhati <bhati.hitender@gmail.com>
2023-03-02 07:30:03 +0530

    Merge branch 'main' of https://github.com/hitender5620/hitender5620

--- virtual_trial_room/models.py
+++ virtual_trial_room/models.py
@@ -1,8 +1,2 @@
 from django.db import models
 
-class posts(models.Model):
-    title = models.TextField()
-    cover = models.ImageField(upload_to="images/")
-    
-def__str__(self):
-    return self.title
