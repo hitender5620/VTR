e7397f43ce829c7e083d3c1199ff7788fa7b4e81
hitender bhati <bhati.hitender@gmail.com>
2023-03-09 14:48:32 +0530

    today updated

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from virtual_trial_room.views import home
 from .views import *
 from django.contrib.auth.views import LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('',home, name= 'home'),
@@ -10,3 +14,6 @@ urlpatterns = [
     path('login/', myloginView.as_view(),name ='login'),
     path('logout/', LogoutView.as_view(next_page='/login/'),name ='logout'),
 ]
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
